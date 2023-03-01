94c8e3597107b7cbf608bd593955155e88cd169e
David Bašić <d.basic97.db@gmail.com>
2023-03-01 20:06:29 +0100

    format

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,8 @@
-import './ExpenseDate.css';
+import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
-    const month = props.date.toLocaleString('en-UK', { month: 'long' });
-    const day = props.date.toLocaleString('en-UK', { day: '2-digit' });
+    const month = props.date.toLocaleString("en-UK", { month: "long" });
+    const day = props.date.toLocaleString("en-UK", { day: "2-digit" });
     const year = props.date.getFullYear();
 
     return (
